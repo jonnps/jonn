7bcf601f0565f24f3c788707612957cfcc343319
Jonnatha Santos <j@jonn.dev>
2025-12-26 12:39:43 -0300

    post: where does the self com from

---
+++ app/components/content/Epigraph.vue
@@ -0,0 +1,6 @@
+<template>
+  <p class="text-right text-sm italic text-gray-400 border-r-2 border-gray-600 pr-4 ml-auto max-w-lg mb-6 leading-relaxed">
+    <slot />
+  </p>
+</template>
+
---
+++ app/components/content/Postscript.vue
@@ -0,0 +1,8 @@
+<template>
+  <aside class="mt-12 pt-6 border-t border-gray-700/50">
+    <p class="text-sm text-gray-500 italic leading-relaxed">
+      <slot />
+    </p>
+  </aside>
+</template>
+
